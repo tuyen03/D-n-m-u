7e8080b06dedc3545496c3f37e8f4e2c2ec09c89
tuyen03 <117988757+tuyen03@users.noreply.github.com>
2023-02-21 18:07:46 +0700

    init project

---
+++ admin/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="boxcenter">
+        <div class="row mb headeradmin">
+            <h1>ADMIN</h1>
+        </div>
+        <div class="row mb menu">
+            <ul>
+                <li><a href="admin.html">Trang chủ</a></li>
+                <li><a href="#">Danh mục</a></li>
+                <li><a href="#">Hàng hóa</a></li>
+                <li><a href="#">Khách hàng</a></li>
+                <li><a href="#">Bình luận</a></li>
+                <li><a href="#">Thống kê</a></li>
+            </ul>
+        </div>
---
+++ admin/home.php
@@ -0,0 +1,23 @@
+<div class="row">
+            <div class="row formtitle">
+                <h1>THÊM MỚI LOẠI HÀNG HÓA</h1>
+            </div>
+            <div class="row formcontent">
+                <form action="#" method="post">
+                    <div class="row mb10">
+                        Mã loại <br>
+                        <input type="text" name="maloai" placeholder="auto number" disabled>
+                    </div>
+                    <div class="row mb10">
+                        Tên loại <br>
+                        <input type="text" name="tenloai">
+                    </div>
+                    <div class="row mb10">
+                        <input type="submit" value="THÊM MỚI">
+                        <input type="reset" value="NHẬP LẠI">
+                      <a href="ds_loaihang.html"><input type="button" value="DANH SÁCH"></a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
